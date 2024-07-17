daf2b55e12979c877e16843de194e9f9708efae8
tellmebaby <tellmebaby407@gmail.com>
2024-07-17 18:20:44 +0900

    dart_input

---
+++ input_widget/lib/screen/join_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class JoinScreen extends StatefulWidget {
+  const JoinScreen({super.key});
+
+  @override
+  State<JoinScreen> createState() => _JoinScreenState();
+}
+
+class _JoinScreenState extends State<JoinScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("회원가입 화면"),
+    );
+  }
+}
