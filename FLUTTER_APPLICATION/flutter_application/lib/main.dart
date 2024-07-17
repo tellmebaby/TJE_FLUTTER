3f53357f114fc3aef87deee76c9d457a857609ea
tellmebaby <tellmebaby407@gmail.com>
2024-07-17 18:19:51 +0900

    flutter_application

---
+++ FLUTTER_APPLICATION/flutter_application/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application/example/text_widget.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        debugShowCheckedModeBanner: false,
+        title: '기본 위젯',
+        theme: ThemeData(
+          colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+          useMaterial3: true,
+        ),
+        home: TextWidget());
+  }
+}
